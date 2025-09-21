e470084eed0fd6807920acb041ba3c99baf86eda
Mick Sayson <mick@sayson.com>
2025-09-21 13:51:50 -0700

    Update default parse network
    With this network we achieve better accuracy with faster execution and
    smaller weight sizes. We have an example of getting 100% correct in
    scanner.zig after 4 hours of training. The network has not peaked yet,
    but training for longer at this stage of development is a waste of time
    
    Our weight sizes are still ~10M, which is very large. It seems like we
    can cut this down more, but this is already a 15x reduction almost, so
    take the win we have
    
    Changes made...
    * Re-jig data generation to pick any arbitrary orientation in the center
      of an image. Size the image such that we are ~ equivalent to a
      1024x1024 image with default_bbox params
    * Enable extraction. Randomization parameters are fairly arbitrary, just
      pick stuff that looks about right
    * Extracted size picked such that
      * Initial input looks reasonable to my human eyes
      * Pooling brings us to a 95x6 image before our fully connected layer
        * Assumption here is that we still want some vertical data for
          global reasoning, but most of it can be pre-processed by earlier
          convolutional layers such that we don't need a ton
    * Pool vertically more, and more often

--- configs/default_parse.py
+++ configs/default_parse.py
@@ -3,30 +3,43 @@
 import json
 import math
 
-batch_size = 40;
-img_size = 380
-conv_end_size = img_size / 4 * img_size / 4 * 8
+batch_size = 96;
+img_width = 380
+img_height = 96
+conv_end_size = img_width / 4 * img_height / 16 * 8
 rand_params = {
-    "x_offs_range": [ -20 * 2 / 3, 20  * 2 / 3],
-    "y_offs_range": [ -20 * 2 / 3, 20  * 2 / 3],
-    "x_scale_range": [ 0.85, 1.0 ],
-    "rot_range": [ -math.pi / 16, math.pi / 16 ],
-    "aspect_range": [0.7, 1.3],
+    "x_offs_range": [ 0, 0 ],
+    "y_offs_range": [ 0, 0 ],
+    "x_scale_range": [ 0.47, 0.71 ],
+    "rot_range": [ -math.pi, math.pi ],
+    "aspect_range": [ 0.4, 1.0 ],
     "min_contrast": 0.5,
-    "x_noise_multiplier_range": [ 1, 5],
-    "y_noise_multiplier_range": [ 1, 5],
-    "perlin_grid_size_range": [ 1, 100 ],
+    "x_noise_multiplier_range": [ 0, 0 ],
+    "y_noise_multiplier_range": [ 0, 0 ],
+    "perlin_grid_size_range": [ 10, 100 ],
     "background_color_range": [ 0.0, 1.0 ],
-    "blur_stddev_range": [ 0.0001, 9.0 ],
+    "blur_stddev_range": [ 0.0001, 0.15 ],
     "no_code_prob": 0.0,
 }
 config = {
     "data": {
         "batch_size": batch_size,
-        "render_size": img_size,
         "label_in_frame": False,
+        "render_size": 866,
         "confidence_metric": "none",
         "rand_params": rand_params,
+        "extract_params": {
+            "extract_width": img_width,
+            "extract_height": img_height,
+
+            "width_err_range": [ -0.10, 0.10 ],
+            "height_err_range": [  -0.1, 0.1 ],
+            "x_err_range": [ -0.03, 0.03 ],
+            "y_err_range": [ -0.03, 0.03 ],
+            "rot_err_range": [ -math.pi / 16, math.pi / 16],
+            "multisample": 4,
+            "dilation": 1.1,
+        },
         "val_rand_params": rand_params,
         "enable_backgrounds": True,
     },
@@ -49,12 +62,14 @@ config = {
             { "relu": 0.1 },
             { "conv": ["he", 3, 3, 4, 8] },
             { "relu": 0.1 },
-            { "maxpool": [2, 2] },
+            { "maxpool": [2, 4] },
 
             { "conv": ["he", 1, 1, 8, 4] },
             { "relu": 0.1 },
             { "conv": ["he", 3, 3, 4, 8] },
             { "relu": 0.1 },
+            { "maxpool": [1, 2] },
+
             { "conv": ["he", 1, 1, 8, 4] },
             { "relu": 0.1 },
             { "conv": ["he", 3, 3, 4, 8] },
